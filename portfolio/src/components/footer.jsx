29d9092876de9fcd7890bb6d9bf676fc08913e64
Donkey <danglayers@gmail.com>
2021-03-11 21:43:33 -0500

    started created components

---
+++ portfolio/src/components/footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => (
+    <footer className="footer">
+        <div className="container">
+            <p className="text-muted text-center">Dan Ayers 2020</p>
+        </div>
+    </footer>
+);
+
+
+export default Footer;
